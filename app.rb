9b2c4eac97c62946f277be097c1269c73c5cc3ca
Ron Sala <ron.sala@gmail.com>
2018-07-06 06:25:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,7 +4,11 @@ module FormsLab
   class App < Sinatra::Base
 
     get '/' do
-      
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
+
+    get '/new' do
+      erb :new
     end
 
   end
